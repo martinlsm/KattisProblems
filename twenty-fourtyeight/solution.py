614c617460f368fd324d0cdeb3d409d8f4b93411
martinlsm <martin.lindstrom94@gmail.com>
2018-06-05 23:45:44 +0200

    Finalized solution for 2048

--- twenty-fourtyeight/solution.py
+++ twenty-fourtyeight/solution.py
@@ -1,18 +1,23 @@
+import sys
+
+
 LEFT = 0
 UP = 1
 RIGHT = 2
 DOWN = 3
 
 
-def parse_to_matrix(input_file):
-    matrix = input_file.read().split('\n')[:4]
-    for i in range(len(matrix)):
-        matrix[i] = list(map(int, matrix[i].split(' ')))
-    return matrix
+def parse_to_matrix():
+    matrix = []
+    for i in range(4):
+        matrix.append(list(map(int, sys.stdin.readline()[:-1].split(' '))))
+    dir = int(sys.stdin.readline())
+    return matrix, dir
 
 
 def print_matrix(matrix):
-    pass
+    for row in matrix:
+        print('{} {} {} {}'.format(*row))
 
 
 def transpose(matrix):
@@ -26,6 +31,7 @@ def transpose(matrix):
 def move_and_merge(vector, dir):
     if dir == RIGHT:
         vector.reverse()
+    #remove zero padding
     for i in range(len(vector)):
         move_count = 0
         for k in range(i):
@@ -36,20 +42,42 @@ def move_and_merge(vector, dir):
         if move_count != 0:
             vector[i - move_count] = vector[i]
             vector[i] = 0
+    #merge duplicates
+    for i in range(len(vector)-1):
+        if vector[i] == vector[i+1]:
+            vector[i] *= -2
+            vector[i+1] = 0
+            for k in range(i+1,len(vector)-1):
+                vector[k] = vector[k+1]
+            vector[-1] = 0
+    #remove negative signs
+    for i in range(len(vector)):
+        vector[i] = abs(vector[i])
     if dir == RIGHT:
         vector.reverse()
 
 
 def make_turn(input_matrix, direction):
-    pass
+    if direction == LEFT:
+        for row in input_matrix:
+            row = move_and_merge(row, LEFT)
+    if direction == UP:
+        transpose(input_matrix)
+        for row in input_matrix:
+            row = move_and_merge(row, LEFT)
+        transpose(input_matrix)
+    if direction == RIGHT:
+        for row in input_matrix:
+            row = move_and_merge(row, RIGHT)
+    if direction == DOWN:
+        transpose(input_matrix)
+        for row in input_matrix:
+            row = move_and_merge(row, RIGHT)
+        transpose(input_matrix)
+    #return input_matrix
 
 
 if __name__ == '__main__':
-    file = open('Samples/In/1.in', 'r')
-    matrix = parse_to_matrix(file)
-    print(matrix)
-    transpose(matrix)
-    print(matrix)
-    vector = [4,4,0,2]
-    move_and_merge(vector, LEFT)
-    print(vector)
+    matrix, dir = parse_to_matrix()
+    make_turn(matrix, dir)
+    print_matrix(matrix)
