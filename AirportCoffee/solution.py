0366f1580d1282cc6339cabb99c98d6f7162b3e7
martinlsm <martin.lindstrom94@gmail.com>
2018-06-06 17:07:31 +0200

    Initial thoughts for Airport Coffe.

---
+++ AirportCoffee/solution.py
@@ -0,0 +1,11 @@
+
+# Change these constants later
+COFFEE_SPEED = 138
+THIRSTY_SPEED = 100
+COOLDOWN_TIME = 60
+DRINK_TIME = 300
+
+
+# returns true or false whether this coffee stop is worth chosing
+def worth_buying(this_stop, next_stop, coffee_dist_left):
+    pass
