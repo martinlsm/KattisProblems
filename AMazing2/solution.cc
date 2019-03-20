8e927891aafcdb3437228b0e0c7b48b1ed026e03
Martin Lindström <martin.lindstrom94@gmail.com>
2019-03-20 21:21:27 +0100

    The maze solver will not turn unless its forced to.

--- AMazing2/solution.cc
+++ AMazing2/solution.cc
@@ -10,7 +10,7 @@ using std::vector;
 using std::pair;
 using std::string;
 using mazepos = pair<short, short>;
-using dirmap = map<mazepos, vector<bool>>;
+using dirmap = map<mazepos, vector<char>>;
 
 const size_t RIGHT = 0;
 const size_t UP    = 1;
@@ -31,31 +31,46 @@ string dir_str(size_t dir) {
 	throw string{"invalid dir index"};
 }
 
+void turn_left(size_t& dir) {
+	dir = (dir + 1) % 4;
+}
+
+void turn_right(size_t& dir) {
+	dir = (dir - 1) % 4;
+}
+
+void go_back(size_t& dir) {
+	dir = (dir + 2) % 4;
+}
+
 int main() {
 	dirmap dirs;
 	dirs.insert({{0, 0}, {0, 0, 0, 0}});
 	dirmap::iterator currpos = dirs.begin();
+	size_t currdir = UP;
 	while (true) {
 		std::vector<size_t> dirsleft;
 		for (size_t i = 0; i < 4; i++) {
-			if (!currpos->second[i]) {
+			if (currpos->second[i] < 2) {
 				dirsleft.push_back(i);
 			}
 		}
 		if (dirsleft.size() == 0) {
 			std::cout << "no way out" << std::endl;
 			break;			
+		} else if (std::find(dirsleft.begin(), dirsleft.end(), currdir) == dirsleft.end()) {
+			currdir = dirsleft[rand() % dirsleft.size()];
 		}
-		size_t rand_dir = dirsleft[rand() % dirsleft.size()];
-		currpos->second[rand_dir] = true;
-		std::cout << dir_str(rand_dir) << std::endl;
+		std::cout << dir_str(currdir) << std::endl;
 		string judge_response;
 		std::cin >> judge_response;
 		if (judge_response == "wall") {
+			currpos->second[currdir] = 2;
 			continue;
 		} else if (judge_response == "ok") {
+			++currpos->second[currdir];
 			mazepos newpos = currpos->first;	
-			switch (rand_dir) {
+			switch (currdir) {
 				case RIGHT:
 					++newpos.first;	
 					break;
@@ -73,9 +88,9 @@ int main() {
 		} else if (judge_response == "solved") {
 			break;
 		} else if (judge_response == "wrong") {
-			throw string{"error, got \"wrong\" from the judge"};
+			return 1;
 		} else {
-			throw string{"received incorrect reponse from the judge"};
+			return 2;
 		} 
 	}
 }
