6163e4304d111f1253406b63ab59b1d012a9c03c
Martin Lindström <martin.lindstrom94@gmail.com>
2020-07-21 19:56:04 +0200

    Add solution to Cetvrta

---
+++ Cetvrta/solution.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a1,a2,a3,b1,b2,b3;
+	cin >> a1 >> b1 >> a2 >> b2 >> a3 >> b3;
+	if (a1 == a2) {
+		cout << a3;
+	} else if (a1 == a3) {
+		cout << a2;
+	} else {
+		cout << a1;
+	}
+	cout << " ";
+
+	if (b1 == b2) {
+		cout << b3;
+	} else if (b1 == b3) {
+		cout << b2;
+	} else {
+		cout << b1;
+	}
+	cout << "\n";
+}
